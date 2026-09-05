2ab60913652ff997e39a42cc1ad8c23b0d4515e2
Fahad Khan <mfahadsk06@gmail.com>
2026-09-05 10:36:39 +0500

    Changing the Code from class based components to function based components

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,124 +1,97 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-export class NavBar extends Component {
-  state = {
-    showNavbar: true,
-    lastScrollY: 0,
-  };
+const Navbar = () => {
+  const [showNavbar, setShowNavbar] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
 
-  componentDidMount() {
-    window.addEventListener("scroll", this.handleScroll);
-  }
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
 
-  componentWillUnmount() {
-    window.removeEventListener("scroll", this.handleScroll);
-  }
+      if (currentScrollY > lastScrollY && currentScrollY > 80) {
+        setShowNavbar(false);
+      } else {
+        setShowNavbar(true);
+      }
+      setLastScrollY(currentScrollY);
+    };
 
-  handleScroll = () => {
-    const currentScrollY = window.scrollY;
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [lastScrollY]);
 
-    if (currentScrollY > this.state.lastScrollY && currentScrollY > 80) {
-      this.setState({ showNavbar: false, lastScrollY: currentScrollY });
-    } else {
-      this.setState({ showNavbar: true, lastScrollY: currentScrollY });
-    }
-  };
-
-  closeNavbar = () => {
+  const closeNavbar = () => {
     const collapseEl = document.getElementById("navbarSupportedContent");
     if (collapseEl && collapseEl.classList.contains("show")) {
-      // eslint-disable-next-line no-undef
-      const bsCollapse = window.bootstrap.Collapse.getInstance(collapseEl) 
-        || new window.bootstrap.Collapse(collapseEl, { toggle: false });
+      const bsCollapse =
+        window.bootstrap.Collapse.getInstance(collapseEl) ||
+        new window.bootstrap.Collapse(collapseEl, { toggle: false });
       bsCollapse.hide();
     }
   };
 
-  render() {
-    return (
-      <div>
-        <nav
-          className="navbar navbar-expand-lg navbar-dark bg-dark"
-          style={{
-            position: "fixed",
-            top: 0,
-            left: 0,
-            right: 0,
-            zIndex: 1030,
-            transition: "transform 0.3s ease-in-out",
-            transform: this.state.showNavbar
-              ? "translateY(0)"
-              : "translateY(-100%)",
-          }}
-        >
-          <div className="container-fluid">
-            <Link className="navbar-brand" to="/">
-              NewsPulse
-            </Link>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <Link className="nav-link" aria-current="page" to="/" onClick={this.closeNavbar}>
-                    Home
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/business" onClick={this.closeNavbar}>
-                    Business
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/entertainment" onClick={this.closeNavbar}>
-                    Entertainment
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/general" onClick={this.closeNavbar}>
-                    General
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/health" onClick={this.closeNavbar}>
-                    Health
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/science" onClick={this.closeNavbar}>
-                    Science
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/sports" onClick={this.closeNavbar}>
-                    Sports
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/technology" onClick={this.closeNavbar}>
-                    Technology
-                  </Link>
-                </li>
-              </ul>
-            </div>
+  return (
+    <div>
+      <nav
+        className="navbar navbar-expand-lg navbar-dark bg-dark"
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          right: 0,
+          zIndex: 1030,
+          transition: "transform 0.3s ease-in-out",
+          transform: showNavbar ? "translateY(0)" : "translateY(-100%)",
+        }}
+      >
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            NewsPulse
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link" aria-current="page" to="/" onClick={closeNavbar}>Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/business" onClick={closeNavbar}>Business</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/entertainment" onClick={closeNavbar}>Entertainment</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/general" onClick={closeNavbar}>General</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/health" onClick={closeNavbar}>Health</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/science" onClick={closeNavbar}>Science</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/sports" onClick={closeNavbar}>Sports</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/technology" onClick={closeNavbar}>Technology</Link>
+              </li>
+            </ul>
           </div>
-        </nav>
-      </div>
-    );
-  }
-}
+        </div>
+      </nav>
+    </div>
+  );
+};
 
-export default NavBar;
+export default Navbar;
--- src/Components/News.js
+++ src/Components/News.js
@@ -1,97 +1,69 @@
-import React, { Component } from "react";
+import React, {useEffect, useState} from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
 
 
-export class News extends Component {
-  static defaultProps = {
-    country: "us",
-    pageSize: 8,
-    category: "general",
-  };
+const News = (props) => {
+   const [articles, setArticles] = useState([]);
+   const [loading, setLoading] = useState(true);
+   const [page, setPage] = useState(1);
+   const [totalResults, setTotalResults] = useState(0);
+  // document.title = `${capitalizeFirstLetter(props.category)} - NewsPulse`;
 
-  static propTypes = {
-    country: PropTypes.string,
-    pageSize: PropTypes.number,
-    category: PropTypes.string,
-  };
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      articles: [],
-      page: 1,
-      totalResults: 0,
-    };
-    document.title = `${this.capitalizeFirstLetter(this.props.category)} - NewsPulse`;
-  }
-
-    capitalizeFirstLetter = (string) => {
+  const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
 
-  async updateNews() {
-    this.props.setProgress(10);
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true });
+  const updateNews = async () => {
+    props.setProgress(10);
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+    setLoading(true);
     let data = await fetch(url);
     let parsedData = await data.json();
-    this.props.setProgress(70);
-    this.setState({
-      articles: parsedData.articles,
-      totalResults: parsedData.totalResults,
-      loading: false,
-    });
-    this.props.setProgress(100);
+    props.setProgress(70);
 
-  }
-
-  async componentDidMount() {
-    this.updateNews();
-  }
-
-  handlePreviousClick = async () => {
-    this.setState({ page: this.state.page - 1 });
-    this.updateNews();
-  };
-
-  handleNextClick = async () => {
-    this.setState({ page: this.state.page + 1 });
-    this.updateNews();
-  };
- 
-  fetchMoreData = async () => {
+    setArticles(parsedData.articles);
+    setTotalResults(parsedData.totalResults);
+    setLoading(false);
    
-    this.setState({ page: this.state.page + 1 }, async () => {
-      const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    props.setProgress(100);
+
+  }
+
+  useEffect(() => {
+    document.title = `${capitalizeFirstLetter(props.category)} - NewsPulse`;
+    updateNews();
+    // eslint-disable-next-line
+  }, [])
+ 
+  const fetchMoreData = async () => {
+    const nextPage = page + 1;
+    setPage(nextPage);
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${nextPage}&pageSize=${props.pageSize}`;
     let data = await fetch(url);
     let parsedData = await data.json();
-      this.setState({
-      articles: this.state.articles.concat(parsedData.articles),
-      totalResults: parsedData.totalResults,
-      loading: false,
-      });
-    });
+    setArticles(articles.concat(parsedData.articles));
+    setTotalResults(parsedData.totalResults);
+    setLoading(false);
   };
 
-  render() {
     return (
       <>
         <h1 className="text-center" style={{ margin: "35px 0", marginTop: "90px" }}>
-          NewsPulse - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
-        {this.state.loading && <Spinner />}
+          NewsPulse - Top {capitalizeFirstLetter(props.category)} Headlines</h1>
+        {loading && <Spinner />}
 
         <InfiniteScroll
-          dataLength={this.state.articles.length}
-          next={this.fetchMoreData}
-          hasMore={this.state.articles.length !== this.state.totalResults}
+          dataLength={articles.length}
+          next={fetchMoreData}
+          hasMore={articles.length !== totalResults}
           loader={<Spinner/>}
         >
         <div className="container">
   <div className="row">
-    {this.state.articles.map((element) => {
+    {articles.map((element) => {
       return (
         <div className="col-md-3" key={element.url}>
           <NewsItem
@@ -115,7 +87,18 @@ export class News extends Component {
         </InfiniteScroll>  
         </>
     );
-  }
-}
+};
+
+News.defaultProps = {
+    country: "us",
+    pageSize: 8,
+    category: "general",
+  };
+
+  News.propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+  };
 
 export default News;
